c85a1e6f83f0ce71e12c33e52d5b233c1e5a1ccf
Tanuj Vishnoi <vishnoitanuj@outlook.com>
2018-11-23 23:51:30 +0530

    chain complete

--- blockchain.py
+++ blockchain.py
@@ -50,6 +50,36 @@ class Blockchain(object):
         })
         
         return self.last_block['index']+1
+
+
+    def proof_of_work(self,last_proof):
+         """
+        Simple Proof of Work Algorithm:
+         - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+         - p is the previous proof, and p' is the new proof
+        :param last_proof: <int>
+        :return: <int>
+        """
+        
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not.
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
     
 
     @staticmethod
