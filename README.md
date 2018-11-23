b1c35cd9c323ee12c2dfffb0465cfa2a3863bc2b
Tanuj Vishnoi <vishnoitanuj@outlook.com>
2018-11-23 16:51:45 +0530

    Blockchain skeleton done

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ### 1) Basics
 Blockchain is an immutable, sequential chain of records called Blocks. They can contain transactions, files or any data you like, really. But the important thing is that they’re chained together using hashes.
 
+### 2) A block
+Each Block has an index, a timestamp (in Unix time), a list of transactions, a proof (more on that later), and the hash of the previous Block.
---
+++ blockchain.py
@@ -0,0 +1,23 @@
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []                 #Empty list to store blockchain
+        self.current_transaction = []
+    
+    def new_block(self):
+        #Creates a new block and adds it to the chain
+        pass
+    
+    def new_transaction(self):
+        #Adds a new transaction to the list of transactions
+        pass
+    
+
+    @staticmethod
+    def hash(block):
+        #Hashes a Block
+    
+
+    @property
+    def last_block(self):
+        #Returns the last Block in the chain
+        pass
