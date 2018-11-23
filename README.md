70fed402de6d6f621288e088f7870b300ba518b3
Tanuj Vishnoi <vishnoitanuj@outlook.com>
2018-11-23 17:12:11 +0530

    genesis done

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-# Getiing Started with Blockchain
+# Getting Started with Blockchain
 
 ### 1) Basics
 Blockchain is an immutable, sequential chain of records called Blocks. They can contain transactions, files or any data you like, really. But the important thing is that they’re chained together using hashes.
 
 ### 2) A block
 Each Block has an index, a timestamp (in Unix time), a list of transactions, a proof (more on that later), and the hash of the previous Block.
+
+### 3) Creating new Blocks
+When our Blockchain is instantiated we’ll need to seed it with a genesis block—a block with no predecessors. We’ll also need to add a “proof” to our genesis block which is the result of mining (or proof of work). 
--- blockchain.py
+++ blockchain.py
@@ -1,23 +1,71 @@
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []                 #Empty list to store blockchain
         self.current_transaction = []
+
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
     
-    def new_block(self):
+    def new_block(self, proof, previous_hash=None):
         #Creates a new block and adds it to the chain
-        pass
+        '''
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+
+        '''
+
+        block = {
+            'index' : len(self.chain) + 1,
+            'timestamp': time()
+            'transactions': self.current_transaction,
+            'proof': proof,
+            'previous_hash' : previous_hash or self.hash(self.chain[-1]),
+        }
+        
+        #Reset the current list of transactions
+        self.current_transaction = []
+
+        self.chain.append(block)
+        return block
     
-    def new_transaction(self):
+    def new_transaction(self, sender, recipient, amount):
         #Adds a new transaction to the list of transactions
-        pass
+        """
+        Creates a new transaction to go into the next mined Block
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hold this transaction
+        """
+
+        self.current_transaction.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount' : amount,
+        })
+        
+        return self.last_block['index']+1
     
 
     @staticmethod
     def hash(block):
         #Hashes a Block
+
+        '''
+
+        Creates a SHA-256 hash of a Block
+
+        :param block: <dict>Block
+        :return: <str>
+        '''
     
 
     @property
     def last_block(self):
         #Returns the last Block in the chain
-        pass
+        return self.chain[-1]
