27b65531aee21031dde876319d96fdb6aa248902
Tanuj Vishnoi <vishnoitanuj@outlook.com>
2018-11-26 18:32:24 +0530

    valid chain done

--- blockchain.py
+++ blockchain.py
@@ -126,6 +126,36 @@ class Blockchain(object):
         parsed_url = urlparse(address)
         self.nodes.add(parsed_url.netloc)
 
+    def valid_chain(self, chain):
+        """
+        Determine if a given blockchain is valid
+        :param chain: <list> A blockchain
+        :return: <bool> True if valid, False if not
+        """
+
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index< len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n----------\n")
+            # Check that the hash of the block is correct
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            #Check that the proof of Work is correct
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+
+            last_block = block
+            current_index += 1
+        return True
+
+        def resolve_conflicts(self):
+            
+
 # Instantiate our Node
 app = Flask(__name__)
 
